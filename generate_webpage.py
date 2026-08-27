ef9b839c2a5496445e4819cf65aa1007e43b3318
iestyn-carlam <iestyn@carlamltd.com>
2026-08-27 13:01:11 +0100

    Add ALL_STAFF list for expanding 'Everyone' tag

--- generate_webpage.py
+++ generate_webpage.py
@@ -27,6 +27,15 @@ from zoneinfo import ZoneInfo
 
 import requests
 
+# The full staff roster, used to expand a "Everyone" tag on a row into every
+# individual person. Keep in step with the "Person Name" select options.
+ALL_STAFF = [
+    "Iestyn O'Leary", "Bethan Evans", "Ceri Siggins", "Cerys Pinkman",
+    "Derwena Burt", "Elin Jones", "Euros Llyr Morgan", "Hannah Holton",
+    "Jason Lye-Phillips", "Lara Hughes", "Osian Lewis", "Owain Jones",
+    "Rhodri Lewis", "Wil Williams",
+]
+
 FILENAME_SALT = os.environ.get("FILENAME_SALT")
 if not FILENAME_SALT:
     print(
@@ -115,7 +124,7 @@ def extract_row(page):
     else:
         person_name_text = get_plain_text(person_name_prop.get("rich_text", []))
     if person_name_text:
-        people = [person_name_text]
+        people = ALL_STAFF if person_name_text == "Everyone" else [person_name_text]
     return {
         "title": title,
         "start_date": start_date,
