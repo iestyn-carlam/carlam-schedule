56570da8f0e86b96e0a0ecb8888dd29801f89edb
iestyn-carlam <iestyn@carlamltd.com>
2026-09-14 14:20:45 +0100

    Add 'Dan Rees' to the ALL_STAFF roster

--- generate_grid.py
+++ generate_grid.py
@@ -24,10 +24,10 @@ import requests
 # The full staff roster, used to expand a "Everyone" tag on a row into every
 # individual person. Keep in step with the "Person Name" select options.
 ALL_STAFF = [
-    "Iestyn O'Leary", "Bethan Evans", "Ceri Siggins", "Cerys Pinkman",
-    "Derwena Burt", "Elin Jones", "Euros Llyr Morgan",
-    "Jason Lye-Phillips", "Lara Hughes", "Osian Lewis", "Owain Jones",
-    "Rhodri Lewis", "Wil Williams",
+    "Iestyn O'Leary", "Bethan Evans", "Ceri Siggins", "Cerys Pinkman", 
+    "Derwena Burt", "Elin Jones", "Euros Llyr Morgan", "Jason Lye-Phillips", 
+    "Lara Hughes", "Osian Lewis", "Owain Jones", "Rhodri Lewis", 
+    "Wil Williams", "Dan Rees"
 ]
 from openpyxl import Workbook
 from openpyxl.styles import Alignment, Border, Font, PatternFill, Side
